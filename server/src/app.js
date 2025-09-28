7c7ef5202517f4bf2ad3682823824475d36999a3
Thomas Chacko <106021054+thomas-chacko@users.noreply.github.com>
2025-09-28 09:12:46 +0530

    install dep

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-//app file
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+// Error Handler
+
+export default app;
